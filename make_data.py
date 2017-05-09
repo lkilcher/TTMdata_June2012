475030b41a1e6cc3be1f43c1b23afbdcdb75c5d6
Levi Kilcher <Levi.Kilcher@gmail.com>
2017-05-09 16:55:04 -0600

    Add scripts

---
+++ make_data.py
@@ -0,0 +1,151 @@
+from __future__ import print_function
+import dolfyn.adv.api as avm
+import dolfyn.adp.api as apm
+import numpy as np
+from dolfyn.data.time import date2num
+import datetime
+
+
+files = {
+    'NREL': 'TTM_NRELvector_Jun2012',
+    'PNNL': 'TTM_PNNLvector_Jun2012',
+    'AWAC': 'TTM_AWAC_Jun2012 '
+}
+
+read_raw = True
+#read_raw = False
+
+do_awac = False
+do_awac = True
+
+
+def within(dat, range):
+    return (range[0] < dat) & (dat < range[1])
+
+trange = [date2num(datetime.datetime(2012, 6, 12, 18, 15, 0)),
+          date2num(datetime.datetime(2012, 6, 14, 14, 41, 0))]
+
+for tag, fname in files.iteritems():
+    if tag == 'AWAC':
+        continue
+    if read_raw:
+        dat = avm.read_nortek('ADV/' + fname + '.vec')
+
+        dat.props['lon'] = -122.6858
+        dat.props['lat'] = 48.1515
+        dat.props['height_above_bottom'] = 11  # m
+        dat.props['total_water_depth'] = 58  # m
+        dat.props['z'] = dat.props['height_above_bottom'] - \
+            dat.props['total_water_depth']
+        # Declination taken from http://www.ngdc.noaa.gov/geomag-web/#declination
+        #  Options: IGRF, 48 09.090', 122 41.149', 2012-06-12
+        dat.props['declination'] = 16.79
+        if tag == 'NREL':
+            dat.props['body2head_rotmat'] = np.eye(3)
+            # In meters, in ADV coord-system:
+            dat.props['body2head_vec'] = np.array([0.48, -0.07, -0.27])  # m
+            # Correct motion (+rotate to earth frame):
+
+        dat.save('ADV/' + fname + '.h5')
+    else:
+        print('Loading file {}.h5...'.format(fname))
+        dat = avm.load('ADV/' + fname + '.h5')
+
+    dat.u[~within(dat.u, [-2.3, 0.5])] = np.NaN
+    avm.clean.fillpoly(dat.u, 3, 12)
+    dat.v[~within(dat.v, [-1, 1])] = np.NaN
+    avm.clean.fillpoly(dat.v, 3, 12)
+    dat.w[~within(dat.w, [-1.2, 0.2])] = np.NaN
+    avm.clean.fillpoly(dat.w, 3, 12)
+
+    dat.props.pop('declination')
+    if tag == 'NREL':
+        print('   motion correction + rotating to earth frame...')
+        avm.motion.correct_motion(dat)
+        # Calculate Euler angles
+        dat.pitch, dat.roll, dat.heading = avm.rotate.orient2euler(
+            dat.orientmat)
+    elif tag == 'PNNL':
+        print('   rotating to earth frame...')
+        # Rotate to earth frame
+        avm.rotate.inst2earth(dat)
+
+    print('   subsetting...')
+    # Crop the section of the data where the instrument was on the seafloor.
+    dat = dat.subset(within(dat.mpltime, trange))
+
+    print('   cleaning...')
+    avm.clean.GN2002(dat.u)
+    avm.clean.GN2002(dat.v)
+    avm.clean.GN2002(dat.w)
+
+    print('   saving...')
+    dat.save('ADV/' + fname + '_earth.h5')
+
+    print('   saving binned data...')
+    bnr = avm.TurbBinner(n_bin=5 * 60 * dat.fs, fs=dat.fs)
+    bd = bnr(dat)
+    if tag == 'NREL':
+        bd.add_data('Spec_urot',
+                    bnr.calc_vel_psd(dat.urot, ),
+                    'spec')
+        bd.add_data('Spec_uacc',
+                    bnr.calc_vel_psd(dat.uacc, ),
+                    'spec')
+        bd.add_data('Spec_umot',
+                    bnr.calc_vel_psd(dat.urot + dat.uacc, ),
+                    'spec')
+        bd.add_data('Spec_uraw',
+                    bnr.calc_vel_psd(dat.uraw, ),
+                    'spec')
+    bd.save('ADV/' + fname + '_earth_b5m.h5')
+
+    print("   saving binned 'principal axes frame' data...")
+    # Rotate to the principal coordinate system:
+    avm.rotate.earth2principal(dat)
+    # Calculate turbulence statistics:
+    bd2 = bnr(dat)
+    # Save `binned` data.
+    if tag == 'NREL':
+        bd2.add_data('Spec_urot',
+                     bnr.calc_vel_psd(dat.urot, ),
+                     'spec')
+        bd2.add_data('Spec_uacc',
+                     bnr.calc_vel_psd(dat.uacc, ),
+                     'spec')
+        bd2.add_data('Spec_umot',
+                     bnr.calc_vel_psd(dat.urot + dat.uacc, ),
+                     'spec')
+        bd2.add_data('Spec_uraw',
+                     bnr.calc_vel_psd(dat.uraw, ),
+                     'spec')
+    bd2.save('ADV/' + fname + '_pax_b5m.h5')
+
+    print("   DONE.")
+
+if do_awac:
+
+    datawac = avm.read_nortek('TTM_AWAC/TTM_AWAC_Jun2012.wpr')
+    datawac = datawac.subset(within(datawac.mpltime, trange))
+    datawac.props['coord_sys'] = 'earth'
+    #datawac.props['time_label'] = tlabel
+    #datawac.props['toff'] = toff
+    # From looking at spectra.
+    datawac.props['doppler_noise'] = {'u': 0.12, 'v': 0.12, 'w': 0.048}
+    datawac.props['fs'] = 1.0
+    datawac.props['n_bin'] = 318  # 5min, 20sec.
+    datawac.save('TTM_AWAC/TTM_AWAC_Jun2012.h5')
+
+    bawac = apm.bin_adcp(datawac, datawac.props['n_bin'])
+    bawac.add_data('Suu', bawac.psd(datawac._u))
+    bawac.save('TTM_AWAC/TTM_AWAC_Jun2012_b5m.h5')
+
+    datawac.calc_principal_angle(10)
+    # The awac principal angle is 180degrees from the adv one.
+    datawac.props['principal_angle'] += np.pi
+    datawac.earth2principal()
+    datawac.save('TTM_AWAC/TTM_AWAC_Jun2012_pax.h5')
+
+    bawac = apm.bin_adcp(datawac, datawac.props['n_bin'])
+    bawac.add_data('Suu', bawac.psd(datawac._u))
+    bawac.save('TTM_AWAC/TTM_AWAC_Jun2012_pax_b5m.h5')
---
+++ make_figs.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+# This script works with dolfyn version 0.3.6
+import ttm.base as pmod
+import matplotlib.pyplot as plt
+import numpy as np
+reload(pmod)
+plt.ion()
+from collections import defaultdict
+import matplotlib.dates as dt
+from calendar import month_name as month
+import ttm.June2012 as j12
+
+flag = defaultdict(lambda: False, {})
+flag['multi_spec01'] = True
+#flag['time01'] = True
+#flag['time_comb'] = True
+#flag['save fig'] = True
+
+tags = [
+    'NREL',
+    #'PNNL',
+]
+
+if 'data' not in vars():
+    data = {}
+print('')
+for ftag in tags:
+    print('Creating figures {}...'.format(ftag))
+    if ftag not in data:
+        print('  Loading data...')
+        data[ftag] = j12.load(ftag, bin=True)
+
+    dat = data[ftag]
+    if flag['multi_spec01']:
+        print('  Creating Spec01...', end=' ')
+        fig, axs = pmod.multi_spec_plot(dat, 1100,
+                                        uranges=np.arange(0, 2.6, 0.5),
+                                        axsize=2,
+                                        noise_level=dict(Spec=[1.6e-4, 1.6e-4, 5e-6],
+                                                         Spec_uraw=[1.6e-4, 1.6e-4, 5e-6]))
+        ax = axs[0, 0]
+        ax.set_xlim([1e-3, 10])
+        ax.set_ylim([1e-5, 1])
+        figfile = 'fig/{}_Spec01.pdf'.format(ftag)
+        print('saving ...', end='')
+        if flag['save fig']:
+            fig.savefig(figfile)
+        print('Done.')
+
+    if flag['time01']:
+        print('  Creating Time01...', end=' ')
+        fig, axs = pmod.vel_time(dat, 1200, )
+        axs[0].set_ylim([-2.5, 2.5])
+        axs[1].set_ylim([-1.0, 1.0])
+        axs[2].yaxis.set_ticks(np.arange(-1, 1, 0.1))
+        axs[2].set_ylim([-0.2, 0.2])
+        axs[2].xaxis.set_major_locator(dt.HourLocator(range(0, 24, 6)))
+        axs[2].xaxis.set_major_formatter(dt.DateFormatter('%d.%H'))
+        axs[2].xaxis.set_minor_locator(dt.HourLocator())
+        axs[2].set_xlabel('Local Time [Day.Hour {}, {}]'
+                          .format(month[dat.mpltime.month[0] + 1], dat.mpltime.year[0]))
+        figfile = 'fig/{}_VelTime01.pdf'.format(ftag)
+        print('saving...', end=' ')
+        if flag['save fig']:
+            fig.savefig(figfile)
+        print('Done.')
+
+    if flag['time_comb']:
+        print('  Creating VelComb01...', end=' ')
+        fig, ax = pmod.vel_time_comb(dat, 1300, )
+        ax.xaxis.set_major_locator(dt.HourLocator(range(0, 24, 6)))
+        ax.xaxis.set_major_formatter(dt.DateFormatter('%d.%H'))
+        ax.xaxis.set_minor_locator(dt.HourLocator())
+        ax.set_xlabel('Local Time [Day.Hour {}, {}]'
+                      .format(month[dat.mpltime.month[0] + 1], dat.mpltime.year[0]))
+        figfile = 'fig/{}_VelComb01.pdf'.format(ftag)
+        print('saving...', end=' ')
+        if flag['save fig']:
+            fig.savefig(figfile)
+        print('Done.')
